d048513cc1d5571f6f35737b2b37cdb00a16088e
matteofab10 <matteofabio10@gmail.com>
2021-11-13 15:40:49 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -3,10 +3,43 @@ const app = new Vue ({
   el: '#app',
 
   data: {
-    
+    itemList: [
+      {
+      text: "Acquistare le scarpe nuove",
+      done: false,
+    },
+    {
+      text: "Portare il giubbotto in lavanderia",
+      done: false,
+    },
+    {
+      text: "Fare la spesa",
+      done: false,
+    }
+    ],
+
+    message:"",
+    error: false
   },
 
   methods: {
-    
-  }
+    removeItem(index){
+      this.itemList.splice(index,1);
+    },
+
+    addItem(){
+      if(this.message.length < 3){
+        this.error = true;
+      } 
+      else
+      {
+        newItem = {
+        text: this.message,
+        done: false,
+        }
+      this.itemList.push(newItem);
+      }
+      this.message ="";
+    },
+  },
 })
